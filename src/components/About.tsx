1198e27ad338d617366e1aa66bcc09a8ae24dbd0
sijibomi <gbemilesanmi@gmail.com>
2023-12-24 21:19:43 +0100

    about page fixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ export default function About() {
     return <section className="w-full h-full bg-black text-white items-center grid grid-cols-1 md:grid-cols-3 px-4 md:px-5 mx-auto">
         <div className=" md:col-span-3 lg:col-span-2  flex items-center h-full">
             <p ref={target} id='target' className={`${manrope.className} text-lg`} >
-            In my quest to use technology for positive change, I'm driven by the idea of making a difference in the world and alleviating human suffering. I see coding as a powerful tool that can go beyond the usual limits and bring about positive transformations globally.
+            In my quest to use technology for positive change, I am driven by the idea of making a difference in the world and alleviating human suffering. I see coding as a powerful tool that can go beyond the usual limits and bring about positive transformations globally.
 I believe coding is a special gift from God almighty, a tool given to us to innovate and create, with the potential to address societal challenges, improve lives, and contribute to the greater good. With this belief, I am motivated to use technology to make a meaningful impact, pushing for progress and contributing to the betterment of humanity.
             </p>
         </div>
