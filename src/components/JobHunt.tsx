8ab8c942d8e017c61e80768f861de1511b0c423e
sijibomi <gbemilesanmi@gmail.com>
2023-12-27 17:58:07 +0100

    exlint errors cleared

--- src/components/JobHunt.tsx
+++ src/components/JobHunt.tsx
@@ -18,7 +18,7 @@ export default function JobHunt() {
     return <section className="w-full h-full bg-black text-white items-center flex px-4 md:px-5 mx-auto">
         <div className=" w-full md:w-2/3 flex items-center h-full">
             <p id='target' className={`${manrope.className} text-xl`} >
-                this is Oluwasijibomi Ilesanmi's homepage, <SpanWord word={"Oluwasijibomi"} /> is software developer in Lagos, Nigeria that likes interacting with and developing for the world wide web, he is genuinely interested in developing software. Soon to be a graduate from Babcock University he is currently looking for full-time opportinities, freelance work or collaborative projects. Contact him via <SpanWord word="Twitter/X" link="https://www.twitter.com/sijisaidwhat" /> or <SpanWord word="Github" link="https://github.com/sijirama" /> or even <SpanWord word="LinkedIn" link="https://www.linkedin.com/in/oluwasijibomi-ilesanmi-8504b123a/" /> although he despises it.
+                this is Oluwasijibomi Ilesanmis homepage, <SpanWord word={"Oluwasijibomi"} /> is software developer in Lagos, Nigeria that likes interacting with and developing for the world wide web, he is genuinely interested in developing software. Soon to be a graduate from Babcock University he is currently looking for full-time opportinities, freelance work or collaborative projects. Contact him via <SpanWord word="Twitter/X" link="https://www.twitter.com/sijisaidwhat" /> or <SpanWord word="Github" link="https://github.com/sijirama" /> or even <SpanWord word="LinkedIn" link="https://www.linkedin.com/in/oluwasijibomi-ilesanmi-8504b123a/" /> although he despises it.
             </p>
         </div>
     </section>
