2539947369ff0ee21aeb98d11ec7701cad08d2f2
sijirama <gbemilesanmi@gmail.com>
2024-02-11 02:36:51 +0100

    init application

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,18 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Button } from '@/components/ui/button'
+import { PopoverDemo } from './components/custom/CardPopOver'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+    //const ipcHandle = (): void => window.Electron.ipcRenderer.send('ping')
 
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+    return (
+        <>
+            <p className='text-black text-3xl font-bold underline flex flex-col space-y-3 items-center justify-center'>
+                Hello world Bitches
+                <Button>Hello world</Button>
+                <PopoverDemo />
+            </p>
+        </>
+    )
 }
 
 export default App
