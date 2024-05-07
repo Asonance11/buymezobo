ef3e9e224362ef36ca3612f27b58bb9564bced1d
sijirama <gbemilesanmi@gmail.com>
2024-05-07 01:27:37 +0100

    save your account info

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import SignOut from "@/components/signout";
 import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/lib/authentication";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+    const profile = await getCurrentUser()
+    if (profile) {
+        redirect(`/dashboard`)
+    }
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
             buy me zobo again
-    
-            <SignOut />
+            
+            {
+                profile ?? <SignOut />
+            }
 
             <Button>Click me</Button>
         </main>
