903051b7dfc1ee8facce1597700a4e848a54c780
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-11 15:31:57 +0100

    Data for Accordion in faq page

---
+++ data/index.ts
@@ -0,0 +1,28 @@
+// fill with the right questions and answers
+export const faqs = [
+  {
+    question: "Who uses Buy Me Zobo?",
+    answer:
+      "Anyone with an audience. Youtubers, musicians, podcasters, writers, programmers, nonprofits, cosplayers, you name it.",
+  },
+  {
+    question: "Who uses Buy Me Zobo?",
+    answer:
+      "Anyone with an audience. Youtubers, musicians, podcasters, writers, programmers, nonprofits, cosplayers, you name it.",
+  },
+  {
+    question: "Who uses Buy Me Zobo?",
+    answer:
+      "Anyone with an audience. Youtubers, musicians, podcasters, writers, programmers, nonprofits, cosplayers, you name it.",
+  },
+  {
+    question: "Who uses Buy Me Zobo?",
+    answer:
+      "Anyone with an audience. Youtubers, musicians, podcasters, writers, programmers, nonprofits, cosplayers, you name it.",
+  },
+  {
+    question: "Who uses Buy Me Zobo?",
+    answer:
+      "Anyone with an audience. Youtubers, musicians, podcasters, writers, programmers, nonprofits, cosplayers, you name it.",
+  },
+];
