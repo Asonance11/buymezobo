6c0e14a0fc0a7e4d31a803e70a36a27e07ca6925
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-11 14:19:58 +0100

    Added main layout for marketing so they all have a header

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import MainHeader from "@/components/common/MainHeader";
+import React, { ReactNode } from "react";
+
+// Added this main layout for all marketing so they all have the header
+
+const MarketingLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <MainHeader />
+      {children}
+    </div>
+  );
+};
+
+export default MarketingLayout;
