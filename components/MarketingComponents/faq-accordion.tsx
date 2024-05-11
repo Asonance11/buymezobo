0d1c1edb202c519711031f4ad624ce8a618bb51e
sijirama <gbemilesanmi@gmail.com>
2024-05-11 19:48:47 +0100

    added some edits to asos faq

---
+++ components/MarketingComponents/faq-accordion.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface FaqAccordionProps {
+    index: number;
+    question: string;
+    answer: string;
+}
+
+const FaqAccordionItem = ({ index, question, answer }: FaqAccordionProps) => {
+    return (
+        <AccordionItem
+            value={index.toString()}
+            className="border-none bg-neutral-100 rounded-md px-4 mb-4"
+        >
+            <AccordionTrigger className="font-semibold text-start text-sm lg:text-lg">
+                {question}
+            </AccordionTrigger>
+            <AccordionContent className="text-xs lg:text-base">{answer}</AccordionContent>
+        </AccordionItem>
+    );
+};
+
+export default FaqAccordionItem;
