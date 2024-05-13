b298d996ac306a95fa256df466c2a33d6c9e1190
sijirama <gbemilesanmi@gmail.com>
2024-05-13 10:08:05 +0100

    sidebar stuff

--- components/AdminMenuContent.tsx
+++ components/AdminMenuContent.tsx
@@ -10,6 +10,8 @@ import { MdOutlineExplore } from 'react-icons/md';
 import { PiLayout } from 'react-icons/pi';
 import { PiArrowSquareOutLight } from 'react-icons/pi';
 import { Logo } from './common/Logo';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { BsCashStack } from 'react-icons/bs';
 
 export default function AdminMenuContent() {
 	const pathname = usePathname();
@@ -41,6 +43,15 @@ export default function AdminMenuContent() {
 		},
 	];
 
+	// Define options with categories and routes
+	const categorizedOptions = [
+		{ category: 'Profile', routes: [{ name: 'Settings', route: '/settings', icon: IoSettingsOutline }] },
+		{
+			category: 'Monetization',
+			routes: [{ name: 'Payouts', route: '/payout', icon: BsCashStack }],
+		},
+	];
+
 	return (
 		<main className="w-full h-full bg-white flex flex-col">
 			<div className="h-[3rem] lg:h-[4rem] flex items-center justify-evenly">
@@ -67,6 +78,23 @@ export default function AdminMenuContent() {
 							{option.newTab ? <RedirectIcon className={` text-lg text-zinc-600`} /> : null}
 						</div>
 					</Link>
+				))}
+				{categorizedOptions.map(({ category, routes }, categoryIndex) => (
+					<div key={categoryIndex} className="mt-4">
+						<p className="text-sm font-light text-gray-500 mb-1">{category}</p>
+						{routes.map((route, routeIndex) => (
+							<Link key={routeIndex} href={route.route}>
+								<div
+									className={`mb-2 flex gap-1.5 items-center justify-around transition-all duration-300 py-2.5 px-3.5 rounded-lg ${pathname === route.route ? 'bg-zinc-100' : 'hover:bg-zinc-100'}`}
+								>
+									<route.icon
+										className={` ${pathname === route.route ? 'text-red-500 ' : null} text-2xl `}
+									/>
+									<p className="flex-1 text-zinc-800 text-sm font-normal">{route.name}</p>
+								</div>
+							</Link>
+						))}
+					</div>
 				))}
 			</section>
 		</main>
