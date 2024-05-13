538f649abc1b4c49966e054a86b2214b034b63b3
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-05-14 00:28:28 +0100

    New login and signup forms

--- app/(authentication)/signup/[[...signup]]/page.tsx
+++ app/(authentication)/signup/[[...signup]]/page.tsx
@@ -1,11 +1,93 @@
+'use client';
+
+import { Logo } from '@/components/common/Logo';
+import { TextInput } from '@/components/ui/TextInput';
+import { Button } from '@/components/ui/button';
 import { SignUp } from '@clerk/nextjs';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect, useRef } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const SignUpSchema = z.object({
+	email: z.string().email().min(1, { message: 'This field is required' }),
+	password: z
+		.string()
+		.min(1, { message: 'This field is required' })
+		.max(12, { message: 'Password must not exceed 12 characters' })
+		.trim(),
+});
+
+type SignUpInputType = z.infer<typeof SignUpSchema>;
 
 export default function Page() {
+	const emailRef = useRef<HTMLInputElement | null>(null);
+	const passwordRef = useRef<HTMLInputElement | null>(null);
+
+	const {
+		handleSubmit,
+		control,
+		trigger,
+		reset,
+		watch,
+		formState: { errors },
+	} = useForm<SignUpInputType>({
+		resolver: zodResolver(SignUpSchema),
+		defaultValues: { email: '', password: '' },
+	});
+
+	useEffect(() => {
+		const subscription = watch((value, { name, type }) => console.log(value, name, type));
+		return () => subscription.unsubscribe();
+	}, [watch]);
+
+	useEffect(() => {
+		if (errors.email) {
+			emailRef.current?.focus();
+		} else if (errors.password) {
+			passwordRef.current?.focus();
+		}
+	}, [errors]);
+
+	const onSignUpSubmit: SubmitHandler<SignUpInputType> = async (data) => {
+		const isValid = await trigger();
+		if (isValid) {
+		}
+		reset();
+	};
+
 	return (
-		<section className="min-h-dvh flex flex-col items-center justify-between">
-			<div className="w-full h-20 bg-white"></div>
-			<SignUp />
-			<div className="w-full h-20 bg-white"></div>
+		<section className=" w-full h-[100vh] flex items-center justify-center flex-col">
+			<nav className=" fixed top-3">
+				<Logo />
+			</nav>
+			<form
+				onSubmit={handleSubmit(onSignUpSubmit)}
+				className=" flex flex-col gap-8 w-[30%] px-8 py-16 h-1/2 border-solid border-slate-300 border-[1px] rounded-sm shadow-md"
+			>
+				<Controller
+					name="email"
+					control={control}
+					rules={{ required: true }}
+					render={({ field }) => (
+						<TextInput inputRef={emailRef} label="Email address" {...field} error={errors.email?.message} />
+					)}
+				/>
+				<Controller
+					name="password"
+					control={control}
+					rules={{ required: true }}
+					render={({ field }) => (
+						<TextInput
+							inputRef={passwordRef}
+							label="Password"
+							{...field}
+							error={errors.password?.message}
+						/>
+					)}
+				/>
+				<Button className=" font-semibold self-center w-full ">Sign Up</Button>
+			</form>
 		</section>
 	);
 }
---
+++ components/ui/TextInput.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { CiWarning } from 'react-icons/ci';
+
+import { cn } from '@/utility/style';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	label: string;
+	inputRef?: React.ForwardedRef<HTMLInputElement>;
+	error?: string;
+	required?: boolean;
+}
+
+const TextInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+	return (
+		<div className=" flex flex-col gap-2">
+			<label htmlFor="" className=" font-semibold">
+				{props.label}
+				{props.required && <span className=" text-red-400">*</span>}
+			</label>
+			<input
+				type={type}
+				className={cn(
+					'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+					className,
+				)}
+				ref={props.inputRef}
+				// {...props}
+			/>
+			{props.error && (
+				<small className=" text-red-500 flex items-center gap-2">
+					<CiWarning /> {props.error}
+				</small>
+			)}
+		</div>
+	);
+});
+TextInput.displayName = 'Input';
+
+export { TextInput };
