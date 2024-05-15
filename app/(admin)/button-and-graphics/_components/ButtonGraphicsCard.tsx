0d93520dc69458f14f60cbc87d19b3f751644eb5
sijirama <gbemilesanmi@gmail.com>
2024-05-15 17:29:43 +0100

    i made changes to the qr stuff, we are solid on qr code then

--- app/(admin)/button-and-graphics/_components/ButtonGraphicsCard.tsx
+++ app/(admin)/button-and-graphics/_components/ButtonGraphicsCard.tsx
@@ -1,99 +1,56 @@
 'use client';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
-import { QRMaker } from './QrMaker';
-import { FaXmark } from 'react-icons/fa6';
-import { FaArrowDown } from 'react-icons/fa';
-import { getCurrentUser } from '@/lib/authentication';
-
-interface Option {
-	title: string;
-	image: string;
-	description: string;
-	action: string;
-}
+import { InterfaceType, useInterface } from '@/store/InterfaceStore';
+import { ButtonOption } from '../page';
 
 interface Props {
-	mainOptions: Option[];
+    mainOptions: ButtonOption[];
 }
 
 const ButtonGraphicsCard: FC<Props> = ({ mainOptions }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [selectedOption, setSelectedOption] = useState<Option | null>(null);
+    const { onOpen } = useInterface();
 
-	const openModal = (option: Option) => {
-		setIsModalOpen(true);
-		setSelectedOption(option);
-	};
-	const closeModal = () => {
-		setIsModalOpen(false);
-		setSelectedOption(null);
-	};
+    const onClickButton = (type: InterfaceType | undefined) => {
+        if (type) {
+            onOpen(type);
+        }
+    };
 
-	const downloadQr = () => {
-		getCurrentUser().then((user) => {
-			const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-			const a = document.createElement('a');
-			a.href = canvas.toDataURL('image/png');
-			a.download = `${user?.userName}-qr.png`;
-			a.click();
-		});
-	};
-
-	return (
-		<div className="grid md:grid-cols-1 lg:grid-cols-2 place-items-stretch gap-4 lg:gap-7 xl:gap-9 ">
-			{mainOptions.map((option, index) => (
-				<Card key={index} className="text-center py-3 rounded-xl shadow-md">
-					<CardHeader className="space-y-2">
-						<Image
-							src={option.image}
-							width={180}
-							height={100}
-							alt={option.title}
-							className="object-cover mx-auto cursor-pointer"
-						/>
-					</CardHeader>
-					<CardContent className="p-0 my-3 space-y-2">
-						<CardTitle className="text-2xl font-semibold">{option.title}</CardTitle>
-						<CardDescription className="w-3/4 mx-auto text-zinc-900 text-sm">
-							{option.description}
-						</CardDescription>
-					</CardContent>
-					<CardFooter className="flex justify-center">
-						<button
-							className="border border-zinc-900 py-2 px-5 rounded-3xl text-sm font-semibold hover:-translate-y-1 transition-all duration-100"
-							onClick={() => {
-								if (option.title === 'QR Code') {
-									openModal(option);
-								}
-							}}
-						>
-							{option.action}
-						</button>
-					</CardFooter>
-				</Card>
-			))}
-			{isModalOpen && selectedOption && (
-				<dialog open className="fixed inset-0 z-50 overflow-y-auto rounded-lg ">
-					<div className="flex items-center justify-center w-[300px] h-[320px] overflow-hidden">
-						<div className="fixed inset-0 bg-black opacity-90"></div>
-						<div className="bg-white rounded-lg p-8 z-50">
-							<div className="flex justify-between px-8">
-								<button className="text-black" onClick={downloadQr}>
-									<FaArrowDown className="text-2xl" />
-								</button>
-								<button className="text-black" onClick={closeModal}>
-									<FaXmark className="text-3xl" />
-								</button>
-							</div>
-							<QRMaker />
-						</div>
-					</div>
-				</dialog>
-			)}
-		</div>
-	);
+    return (
+        <div className="grid md:grid-cols-1 lg:grid-cols-2 place-items-stretch gap-4 lg:gap-7 xl:gap-9 ">
+            {mainOptions.map((option, index) => (
+                <Card key={index} className="text-center py-3 rounded-xl shadow-md">
+                    <CardHeader className="space-y-2">
+                        <Image
+                            src={option.image}
+                            width={180}
+                            height={100}
+                            alt={option.title}
+                            className="object-cover mx-auto cursor-pointer"
+                        />
+                    </CardHeader>
+                    <CardContent className="p-0 my-3 space-y-2">
+                        <CardTitle className="text-2xl font-semibold">{option.title}</CardTitle>
+                        <CardDescription className="w-3/4 mx-auto text-zinc-900 text-sm">
+                            {option.description}
+                        </CardDescription>
+                    </CardContent>
+                    <CardFooter className="flex justify-center">
+                        <button
+                            onClick={() => {
+                                onClickButton(option?.type);
+                            }}
+                            className="border border-zinc-900 py-2 px-5 rounded-3xl text-sm font-semibold hover:-translate-y-1 transition-all duration-100"
+                        >
+                            {option.action}
+                        </button>
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+    );
 };
 
 export default ButtonGraphicsCard;
--- providers/InterfaceProvider.tsx
+++ providers/InterfaceProvider.tsx
@@ -2,28 +2,30 @@
 
 import EditUsernamePageModal from '@/components/Interface/EditUsernamePageModal';
 import PayoutInfoModal from '@/components/Interface/PayoutInfoModal';
+import QRCodeModal from '@/components/Interface/QRCodeModal';
 import { SearchCreatorMenu } from '@/components/Interface/SearchCreators';
 import { SideMenuNavigationComponent } from '@/components/Interface/SideMenuNavigationHeader';
 import WithdrawPayoutModal from '@/components/Interface/WithdrawPayoutModal';
 import { useEffect, useState } from 'react';
 export function InterfaceProvider() {
-    const [isMounted, setIsMounted] = useState(false);
+	const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
 
-    if (!isMounted) {
-        return null;
-    }
+	if (!isMounted) {
+		return null;
+	}
 
-    return (
-        <>
-            <WithdrawPayoutModal />
-            <PayoutInfoModal />
-            <SearchCreatorMenu />
-            <SideMenuNavigationComponent />
-            <EditUsernamePageModal />
-        </>
-    );
+	return (
+		<>
+			<QRCodeModal />
+			<WithdrawPayoutModal />
+			<PayoutInfoModal />
+			<SearchCreatorMenu />
+			<SideMenuNavigationComponent />
+			<EditUsernamePageModal />
+		</>
+	);
 }
