a3f13d76aa7c9afbf8e47c13585ae5f4514ba50c
sijirama <gbemilesanmi@gmail.com>
2024-05-16 04:19:30 +0100

    on sign in, access to the auth pages have been removed

--- app/(authentication)/layout.tsx
+++ app/(authentication)/layout.tsx
@@ -1,5 +1,37 @@
+'use client';
 import bg from '/public/images/authbg.svg';
+import Loading from './loading';
+import { useAuth } from '@/actions/use-auth';
+import { useEffect, useState } from 'react';
+import { User } from 'lucia';
+import { useRouter } from 'next/navigation';
 
 export default function ClerkLayout({ children }: { children: React.ReactNode }) {
+	const [profile, setProfile] = useState<User | null>(null);
+	const [loading, setLoading] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		const fetchProfile = async () => {
+			const { user } = await useAuth();
+			setProfile(user);
+			setLoading(false);
+		};
+		fetchProfile();
+	}, []);
+
+	if (loading) {
+		return (
+			<main className="min-h-dvh flex flex-col">
+				<Loading />;
+			</main>
+		);
+	}
+
+	if (profile) {
+		router.push('/dashboard');
+		return null;
+	}
+
 	return <section className="min-h-dvh bg-white">{children}</section>;
 }
