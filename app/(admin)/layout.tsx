7021783c61564892720754cb72ed1aff1c3121d5
sijirama <gbemilesanmi@gmail.com>
2024-05-16 04:15:48 +0100

    admin layout ting

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,7 +1,39 @@
+'use client';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import AdminHeader from '@/components/common/AdminHeader';
+import Loading from './loading';
+import { useAuth } from '@/actions/use-auth';
+import { useEffect, useState } from 'react';
+import { User } from 'lucia';
+import { useRouter } from 'next/navigation';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+	const [profile, setProfile] = useState<User | null>(null);
+	const [loading, setLoading] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		const fetchProfile = async () => {
+			const { user } = await useAuth();
+			setProfile(user);
+			setLoading(false);
+		};
+		fetchProfile();
+	}, []);
+
+	if (loading) {
+		return (
+			<main className="min-h-dvh flex flex-col">
+				<Loading />;
+			</main>
+		);
+	}
+
+	if (!profile) {
+		router.push('/signin');
+		return null;
+	}
+
 	return (
 		<section className="max-h-screen bg-black flex">
 			<Sidebar />
