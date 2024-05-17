dce0bdb5e18bac68140338a84693b32f351d0905
CherishShed <c.shed2000@gmail.com>
2024-05-17 06:56:31 +0100

    wrote api to get one creator

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,7 +1,7 @@
 import { generateUploadButton, generateUploadDropzone } from '@uploadthing/react';
 import { UTApi } from 'uploadthing/server';
 
-import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import type { OurFileRouter } from '@/app/api/support/uploadthing/core';
 import axios from 'axios';
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
