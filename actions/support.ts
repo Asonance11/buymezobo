8c49d824cf330e091aea3e6389aa09d64e3167e4
sijirama <gbemilesanmi@gmail.com>
2024-05-29 12:00:00 +0100

    comments and delete comments

--- actions/support.ts
+++ actions/support.ts
@@ -3,38 +3,39 @@ import { db } from '@/lib/database';
 import { Support } from '@prisma/client';
 
 export async function getSupportbyId(id: string): Promise<Support | null> {
-	try {
-		const support = await db.support.findFirst({
-			where: {
-				id,
-			},
-		});
-		return support;
-	} catch (error) {
-		return null;
-	}
+    try {
+        const support = await db.support.findFirst({
+            where: {
+                id,
+            },
+        });
+        return support;
+    } catch (error) {
+        return null;
+    }
 }
-export async function getCreatorSupports(creatorId: string, take?: number) {
-	try {
-		const supports = await db.support.findMany({
-			where: {
-				profileId: creatorId,
-				deleted: false,
-			},
-			take: take ? take : 10,
-			orderBy: {
-				createdAt: 'desc',
-			},
-			include: {
-				comments: true,
-			},
-		});
+export async function getCreatorSupports(creatorId: string, take?: number, deleted: boolean = false) {
 
-		const count = await db.support.count();
+    try {
+        const supports = await db.support.findMany({
+            where: {
+                profileId: creatorId,
+                deleted: deleted,
+            },
+            take: take ? take : 10,
+            orderBy: {
+                createdAt: 'desc',
+            },
+            include: {
+                comments: true,
+            },
+        });
 
-		return [supports, count, null];
-	} catch (error) {
-		console.log(error);
-		return [[], 0, error as Error];
-	}
+        const count = await db.support.count();
+
+        return [supports, count, null];
+    } catch (error) {
+        console.log(error);
+        return [[], 0, error as Error];
+    }
 }
