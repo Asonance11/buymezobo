11e62b362a0bf8846fc9cc32f68a39f5c7c8b6b2
sijirama <gbemilesanmi@gmail.com>
2024-05-29 16:57:41 +0100

    some changes

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
+import { AboutLocationButton } from '@/components/MarketingComponents/AboutButtonLocation';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { DancingScript, fontSans, helvetica, shadowLight } from '@/utility/fonts';
 import { Metadata } from 'next';
+import { MdOutlineLocationOn } from 'react-icons/md';
 
 export const metadata: Metadata = {
 	title: 'About - Buy Me Zobo',
@@ -9,38 +13,57 @@ export const metadata: Metadata = {
 
 const AboutPage = () => {
 	return (
-		<section className="mt-4 md:mt-16 lg:mt-20 px-4 flex flex-col justify-center mx-auto md:max-w-3xl pb-12 ">
-			<div className="text-left">
-				<h1 className={`${helvetica.className} text-base lg:text-xl font-semibold`}>Our Gist</h1>
+		<main className="mt-4 md:mt-16 lg:mt-20 px-4 gap-6 flex flex-col justify-center mx-auto md:max-w-3xl pb-12 items-center ">
+			<Button variant={'outline'} className="w-fit my-1 ">
+				<MdOutlineLocationOn className="xl" />
+				<hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />
+				<p className="text-sm">Lagos, NG</p>
+			</Button>
+
+			<div className=" flex flex-col items-center gap-3">
+				<h1 className={` ${helvetica.className} text-4xl text-center font-bold w-full lg:w-3/4 mx-auto `}>
+					Welcome to BuyMeZobo, the Naija Creators' Paradise!
+				</h1>
+				<span className="flex items-center text-gray-500 font-light text-center">
+					<p className="text-sm">3 min</p>
+					<hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />
+					<p className="text-sm">Read time</p>
+				</span>
 			</div>
-			<div className={`${fontSans.className} text-sm xl:text-base font-light leading-none`}>
-				<p className="mt-4 mb-3 xl:mb-5">
-					We're firm believers that every Nigerian has a creator within them. Our mission is to support as
-					many Nigerian creators in unlocking their full potential and effortlessly earning from their
-					creativity.
-				</p>
-				<p>
-					Our Goal? Ditch the stress, simplify "appreciations", and foster genuine connections between
-					creators and their fans. But guess what? Simple just won't cut it. Creators craved something more
-					than just ease-of-use; a platform that resonated with meaning and sparked joy. And so, we set out to
-					design a payment experience that goes beyond mere transactions."
-				</p>
-				<p className="mt-6">
-					Ever heard the joke about the Nigerian creator who walked into a bar? Neither have we, because they
-					were too busy crafting their next masterpiece! At our core, we're all about turning creativity into
-					cash, so you can say goodbye to those days of relying on ads that vanish quicker than NEPA power.
-					With Buy Me a Coffee... oops, I mean Buy Me Zobo, Nigerian creators can finally sip on success, one
-					grateful fan at a time. It's time to turn those creative sparks into Naira bills, because why settle
-					for just dreams when you can have your own fan-funded reality show? Let's paint the town green,
-					white, green with creativity, one sip at a time!
-				</p>
-			</div>
+
+			<section className="rounded-xl bg-white w-full lg:w-3/4 border border-zinc-300">
+				<div className="text-left border-b border-zinc-300 p-3">
+					<h1 className={`${helvetica.className} text-base lg:text-xl font-semibold`}>Our Gist</h1>
+				</div>
+				<div className={`${fontSans.className} text-base font-light text-center p-3`}>
+					<p className="mt-4 mb-3 xl:mb-5">
+						We're firm believers that every Nigerian has a creator within them. Our mission is to support as
+						many Nigerian creators in unlocking their full potential and effortlessly earning from their
+						creativity.
+					</p>
+					<p>
+						Our Goal? Ditch the stress, simplify "appreciations", and foster genuine connections between
+						creators and their fans. But guess what? Simple just won't cut it. Creators craved something
+						more than just ease-of-use; a platform that resonated with meaning and sparked joy. And so, we
+						set out to design a payment experience that goes beyond mere transactions."
+					</p>
+					<p className="mt-6">
+						Ever heard the joke about the Nigerian creator who walked into a bar? Neither have we, because
+						they were too busy crafting their next masterpiece! At our core, we're all about turning
+						creativity into cash, so you can say goodbye to those days of relying on ads that vanish quicker
+						than NEPA power. With Buy Me a Coffee... oops, I mean Buy Me Zobo, Nigerian creators can finally
+						sip on success, one grateful fan at a time. It's time to turn those creative sparks into Naira
+						bills, because why settle for just dreams when you can have your own fan-funded reality show?
+						Let's paint the town green, white, green with creativity, one sip at a time!
+					</p>
+				</div>
+			</section>
 			<div className="flex mt-8">
 				<p className={`text-2xl lg:text-3xl tracking-tighter italic select-none ${shadowLight.className}`}>
 					BMZteam
 				</p>
 			</div>
-		</section>
+		</main>
 	);
 };
 
--- app/(marketing)/faq/page.tsx
+++ app/(marketing)/faq/page.tsx
@@ -2,49 +2,46 @@ import FaqAccordionItem from '@/components/MarketingComponents/faq-accordion';
 import { Accordion } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
 import { faqs } from '@/data/faq';
-import { DancingScript } from '@/utility/fonts';
+import { DancingScript, helvetica } from '@/utility/fonts';
 import Link from 'next/link';
 import React, { Suspense } from 'react';
 
 const FaqPage = () => {
-	return (
-		<section className="mt-4 md:mt-16 lg:mt-24 px-4 flex flex-col justify-center mx-auto md:max-w-3xl pb-12">
-			<div className="text-center">
-				<h1 className={`${DancingScript.className} text-2xl lg:text-3xl font-bold`}>
-					Frequently Asked Questions
-				</h1>
-				<p className="mt-6 text-sm lg:text-base">
-					If you can&apos;t find an answer that you&apos;re looking for, feel free to drop us a line.
-				</p>
-			</div>
-			<div className="mt-6 flex items-center justify-center gap-x-4">
-				<Link href="/about">
-					<Button
-						variant="outline"
-						className="border text-sm border-black transition-transform duration-300 rounded-full hover:bg-transparent hover:scale-105"
-					>
-						About the company
-					</Button>
-				</Link>
-				<Link href="">
-					<Button
-						variant="outline"
-						className="border text-sm border-black transition-transform duration-300 rounded-full hover:bg-transparent hover:scale-105"
-					>
-						Contact support
-					</Button>
-				</Link>
-			</div>
+    return (
+        <section className="mt-4 md:mt-16 lg:mt-24 p-4 flex flex-col gap-5 justify-center mx-auto max-w-4xl mmd:max-w-3xl ">
+            <section className="">
+                <div className="text-center">
+                    <h1 className={`${helvetica.className} text-2xl lg:text-3xl font-bold tracking-tight`}>
+                        Frequently Asked Questions
+                    </h1>
+                    <p className=" mt-3 lg:mt-4 text-sm lg:text-base font-light text-center mx-auto w-full lg:w-2/3">
+                        If you can&apos;t find an answer that you&apos;re looking for, feel free to drop us a line,
+                        we'll try to update this as much as we can
+                    </p>
+                </div>
+                <div className="mt-4 flex items-center justify-center gap-x-5">
+                    <Link href="/about">
+                        <Button variant={'outline'} className="font-semibold border-purple-400 text-zinc-700 ">
+                            About the company
+                        </Button>
+                    </Link>
+                    <Link href="mailto:gbemilesanmi@gmail.com">
+                        <Button variant={'outline'} className="font-semibold border-purple-400 text-zinc-700 ">
+                            Contact support
+                        </Button>
+                    </Link>
+                </div>
+            </section>
 
-			<section className="mt-16">
-				{faqs.map((faq, index) => (
-					<Accordion type="multiple" key={index}>
-						<FaqAccordionItem index={index} question={faq.question} answer={faq.answer} />
-					</Accordion>
-				))}
-			</section>
-		</section>
-	);
+            <section className="p-3 w-full lg:w-3/4 mx-auto">
+                {faqs.map((faq, index) => (
+                    <Accordion type="multiple" key={index}>
+                        <FaqAccordionItem index={index} question={faq.question} answer={faq.answer} />
+                    </Accordion>
+                ))}
+            </section>
+        </section>
+    );
 };
 
 export default FaqPage;
