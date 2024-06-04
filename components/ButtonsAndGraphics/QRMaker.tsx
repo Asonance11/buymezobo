f0f3671b5c2d095074353c6d93212ec670cbb242
sijirama <gbemilesanmi@gmail.com>
2024-06-04 10:23:21 +0100

    mini changes

--- components/ButtonsAndGraphics/QRMaker.tsx
+++ components/ButtonsAndGraphics/QRMaker.tsx
@@ -1,20 +1,24 @@
-import { useOrigin } from '@/hooks/useOrigin';
-import { getCurrentUser } from '@/lib/authentication';
-import { User } from 'lucia';
-import { useEffect, useState } from 'react';
+// import { useOrigin } from '@/hooks/useOrigin';
+// import { getCurrentUser } from '@/lib/authentication';
+// import { User } from 'lucia';
+// import { useEffect, useState } from 'react';
+
+import { useUser } from '@/store/UserDataStore';
 import { QRCode } from 'react-qrcode-logo';
 
 export function QRMaker() {
-	const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
-	const origin = useOrigin();
-	useEffect(() => {
-		const fetchProfile = async () => {
-			const loggedInUser = await getCurrentUser();
-			setLoggedInUser(loggedInUser);
-		};
-		fetchProfile();
-	}, []);
+	// const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+	// const origin = useOrigin();
+	// useEffect(() => {
+	// 	const fetchProfile = async () => {
+	// 		const loggedInUser = await getCurrentUser();
+	// 		setLoggedInUser(loggedInUser);
+	// 	};
+	// 	fetchProfile();
+	// }, []);
+
+	const { loggedInUser } = useUser();
 
 	return (
 		<QRCode
@@ -25,7 +29,6 @@ export function QRMaker() {
 			eyeColor={'#7C3BED'}
 			// logoImage="/image.png"
 			removeQrCodeBehindLogo={true}
-			// bgColor="gray"
 		/>
 	);
 }
