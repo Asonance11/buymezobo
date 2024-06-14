b98fe14adf396c03029680df35d8ce24d87a5db4
sijirama <gbemilesanmi@gmail.com>
2024-06-14 11:52:01 +0100

    article comments

--- app/(public)/article/[articleId]/page.tsx
+++ app/(public)/article/[articleId]/page.tsx
@@ -13,15 +13,14 @@ import Editor from '@/components/Editor/Editor';
 import ProfileCardComponent from '@/components/Profile/ComponentCard';
 import Loader from '@/components/common/Loader';
 import { ArticlePrimitive, CommentPrimitive } from '@/types/primitives';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import moment from 'moment';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { truncateText } from '@/utility/text';
 import UserNameHeader from '@/components/Headers/UsernameHeader';
 import { User } from 'lucia';
 import Link from 'next/link';
-import { calculateReadingTime } from '@/utility/articles';
-import { Block } from '@/types/blocknote';
 import { useUser } from '@/store/UserDataStore';
 import { toast } from 'sonner';
 import { Textarea } from '@/components/ui/textarea';
@@ -32,34 +31,75 @@ import { Button } from '@/components/ui/button';
 import { avatarImageUrl } from '@/utility/avatar';
 import { Profile } from '@prisma/client';
 import { SendIcon } from 'lucide-react';
+import queryKeys from '@/query-key-factory';
 
 export default function Page(props: any) {
 	const articleId = props.params.articleId;
 
-	const [article, setArticle] = useState<ArticlePrimitive | null>(null);
-	const [comments, setComments] = useState<CommentPrimitive[]>([]);
-	const [loading, setLoading] = useState<boolean>(false);
-
-	const [isEmojiOpen, setIsEmojiOpen] = useState(true);
-	const [commentInput, setCommentInput] = useState<string>('');
-
 	const { loggedInUser } = useUser();
+	const queryClient = useQueryClient();
 
-	useEffect(() => {
-		const fetchArticle = async (id: string) => {
-			const response = await axios.get(`/api/articles/${id}`);
-			console.log(response.data.article);
-			setArticle(response.data.article);
-			setComments(response.data.article.comments);
-		};
-		if (articleId) {
-			fetchArticle(articleId);
-		}
-	}, [articleId]);
+	const [commentInput, setCommentInput] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const [isEmojiOpen, setIsEmojiOpen] = useState(true);
 
-    // the api route is /api/comments/${articleId}
+	const fetchArticle = async (id: string): Promise<ArticlePrimitive> => {
+		const response = await axios.get(`/api/articles/${id}`);
+		return response.data.article;
+	};
 
-	if (!article) {
+	const fetchComments = async (articleId: string): Promise<CommentPrimitive[]> => {
+		const response = await axios.get(`/api/comments/${articleId}`);
+		return response.data.comments;
+	};
+
+	const {
+		data: article,
+		isLoading: articleLoading,
+		refetch,
+	} = useQuery({
+		queryKey: queryKeys.article.one(articleId),
+		queryFn: () => fetchArticle(articleId),
+	});
+
+	const { data: comments, refetch: refetchComments } = useQuery({
+		queryKey: queryKeys.comments.byArticle(articleId),
+		queryFn: () => fetchComments(articleId),
+	});
+
+	const mutation = useMutation({
+		mutationFn: (newComment: any) =>
+			axios.post(`/api/comments/`, {
+				content: newComment.content,
+				author: loggedInUser,
+				parentId: articleId, // Use articleId directly
+			}),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: [...queryKeys.comments.byArticle(articleId)] });
+			toast.success('Comment submitted');
+			setCommentInput('');
+			refetchComments(); // Ensure comments are refetched after submitting a new comment
+		},
+		onError: () => {
+			toast.error('Failed to submit your comment');
+		},
+	});
+
+	const onCommentSubmit = () => {
+		if (loading) return;
+		setLoading(true);
+		mutation.mutate({ content: commentInput });
+		setLoading(false);
+	};
+
+	const date = article ? moment(article.createdAt).fromNow() : '';
+
+	const onEmojiClick = (emojiObject: EmojiClickData) => {
+		const content = commentInput + emojiObject.emoji;
+		setCommentInput(content);
+	};
+
+	if (articleLoading || !article) {
 		return (
 			<div className="w-full min-h-screen flex items-center justify-center">
 				<Loader />
@@ -67,44 +107,12 @@ export default function Page(props: any) {
 		);
 	}
 
-	const onCommentSubmit = async () => {
-		if (loading) {
-			return;
-		}
-		try {
-			setLoading(true);
-			const response = await axios.post(`/api/comments/`, {
-				content: commentInput,
-				author: loggedInUser,
-				parentId: article.id,
-			});
-
-			const newComment: CommentPrimitive = response.data.comment;
-			setComments([newComment, ...comments]);
-			setCommentInput('');
-
-			toast.success('Comment submitted');
-		} catch (error) {
-			toast.error('Failed to submit your comment');
-		} finally {
-			setLoading(false);
-		}
-	};
-
-	const date = moment(article.createdAt).format('MMMM Do YYYY, h:mm:ss a'); // June 10th 2024, 6:43:57 pm
-
-	const onEmojiClick = (emojiObject: EmojiClickData) => {
-		console.log(emojiObject.emoji);
-		const content = commentInput + emojiObject.emoji;
-		setCommentInput(content);
-	};
-
 	return (
 		<main className="bg-gray-100">
 			<UserNameHeader user={article.profile as User} />
-			<main className=" w-full lg:w-2/4 mx-auto flex flex-col p-2 md:py-6 md:px-16 h-full gap-4 ">
+			<main className="w-full lg:w-2/4 mx-auto flex flex-col p-2 md:py-6 md:px-16 h-full gap-4 ">
 				<section className="space-y-2">
-					<div className=" mx-auto p-4 ">
+					<div className="mx-auto p-4">
 						<Breadcrumb>
 							<BreadcrumbList>
 								<BreadcrumbItem>
@@ -125,14 +133,13 @@ export default function Page(props: any) {
 							</BreadcrumbList>
 						</Breadcrumb>
 					</div>
-					<div className="p-4 space-y-3 ">
+					<div className="p-4 space-y-3">
 						<p className="text-3xl font-bold -tracking-wide">{article.title}</p>
 						<div className="flex items-center justify-start gap-1.5">
 							<div
 								className="cursor-pointer rounded-lg w-10 lg:w-12 h-10 lg:h-12 bg-center bg-cover bg-no-repeat border-1 border-purple-300"
 								style={{ backgroundImage: `url(${article.profile.imageUrl})` }}
 							></div>
-
 							<div>
 								<Link href={`/${article.profile.userName}`}>
 									<p className="hover:underline font-semibold text-gray-800 cursor-pointer">
@@ -143,18 +150,17 @@ export default function Page(props: any) {
 							</div>
 						</div>
 					</div>
-					{article.image ? (
+					{article.image && (
 						<div
-							className="flex gap-3  bg-red-400 h-40 lg:h-56 bg-center bg-cover bg-no-repeat rounded-lg"
+							className="flex gap-3 bg-red-400 h-40 lg:h-56 bg-center bg-cover bg-no-repeat rounded-lg"
 							style={{ backgroundImage: `url(${article.image})` }}
 						></div>
-					) : null}
-
+					)}
 					<div className="py-6 bg-white rounded-lg">
 						<Editor readOnly initialValues={article.content} previewMode />
 					</div>
 				</section>
-				<div className=" max-w-[30rem]">
+				<div className="max-w-[30rem]">
 					<ProfileCardComponent profile={article.profile} className="sticky top-2 hidden" />
 				</div>
 				{!loggedInUser ? (
@@ -167,7 +173,6 @@ export default function Page(props: any) {
 							className="cursor-pointer rounded-lg w-10 lg:w-12 h-10 lg:h-12 bg-center bg-cover bg-no-repeat border-1 border-purple-300"
 							style={{ backgroundImage: `url(${avatarImageUrl(loggedInUser as Profile)})` }}
 						></div>
-
 						<div className="border-gray-200 border rounded-lg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 bg-white flex-1">
 							<Textarea
 								className="resize-none border-none focus:border-none focus-visible:ring-offset-0 focus-visible:ring-0"
@@ -179,7 +184,7 @@ export default function Page(props: any) {
 							<div className="w-full p-2.5 flex items-center justify-end gap-2">
 								<DropdownMenu>
 									<DropdownMenuTrigger className="outline-none">
-										<HiOutlineEmojiHappy className="text-xl md:text-2xl text-gray-500 focus:text-purple-700 " />{' '}
+										<HiOutlineEmojiHappy className="text-xl md:text-2xl text-gray-500 focus:text-purple-700" />
 									</DropdownMenuTrigger>
 									<DropdownMenuContent>
 										<Emoji open={isEmojiOpen} onEmojiClick={onEmojiClick} />
@@ -195,12 +200,12 @@ export default function Page(props: any) {
 						</div>
 					</section>
 				)}
-				{comments.length === 0 && (
+				{comments && comments.length === 0 && (
 					<div className="w-full h-40">
 						<p className="text-center text-gray-600">No comments yet</p>
 					</div>
 				)}
-				{comments.length > 0 && (
+				{comments && comments.length > 0 && (
 					<section className="w-full bg-white p-4 rounded-t-lg">
 						{comments.map((comment) => (
 							<div key={comment.id} className="p-2 flex items-start gap-2">
@@ -215,7 +220,6 @@ export default function Page(props: any) {
 										<ProfileCardComponent profile={comment.profile} />
 									</HoverCardContent>
 								</HoverCard>
-
 								<div className="space-y-1">
 									<div className="flex items-center justify-start gap-2">
 										<Link href={`/${comment.profile.userName}`}>
@@ -228,7 +232,7 @@ export default function Page(props: any) {
 									<div className="text-gray-800 text-sm">{comment.content}</div>
 								</div>
 							</div>
-						))}{' '}
+						))}
 					</section>
 				)}
 			</main>
--- query-key-factory/index.ts
+++ query-key-factory/index.ts
@@ -3,6 +3,8 @@ import { searchCreatorsKeys } from './search-creators.keys';
 import { supportsKeys } from './supports.keys';
 import { userkeys } from './user.keys';
 import { notificationKeys } from './notification.keys';
+import { commentsKeys } from './comments.keys';
+import { articlesKeys } from './article.keys';
 
 const queryKeys = {
 	searchCreators: searchCreatorsKeys,
@@ -10,6 +12,8 @@ const queryKeys = {
 	post: postsKeys,
 	user: userkeys,
 	notification: notificationKeys,
+    comments: commentsKeys,
+    article: articlesKeys
 };
 
 export default queryKeys;
