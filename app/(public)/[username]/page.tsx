4f6611c693e7acded3b12bf50a8c866aa0dd0082
sijirama <gbemilesanmi@gmail.com>
2024-06-20 21:48:28 +0100

    progress

--- app/(public)/[username]/page.tsx
+++ app/(public)/[username]/page.tsx
@@ -15,6 +15,7 @@ import { AboutCard } from './_components/AboutCard';
 import { ImagePostCard } from './_components/ImagePostCard';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { Button } from '@/components/ui/button';
+import { useUser } from '@/store/UserDataStore';
 
 export default function Username(props: any) {
 	const creatorname = props.params.username;
@@ -22,6 +23,8 @@ export default function Username(props: any) {
 	const [reloadSupporters, setReloadSupporters] = useState(false);
 
 	const [tabIndex, setTabIndex] = useState(0);
+
+	const { loggedInUser } = useUser();
 
 	const { data: creator } = useQuery({
 		queryKey: queryKeys.user.getByName(creatorname),
@@ -54,39 +57,36 @@ export default function Username(props: any) {
 	return (
 		<main className="min-h-screen bg-white flex flex-col ">
 			<UserNameHeader className="" user={creator as User} />
-			<section className="flex-1 bg-blue-700 w-full flex flex-col ">
+			<section className="flex-1 bg-purple-100 w-full flex flex-col ">
 				<div
-					className="w-full h-32 lg:h-36 xl:h-56 bg-gray-300 bg-center bg-cover bg-no-repeat border-2 border-red-800 relative"
+					className="w-full h-32 lg:h-36 xl:h-52 bg-gray-300 bg-center bg-cover bg-no-repeat relative"
 					style={{ backgroundImage: `url(${creator?.headerImageUrl})` }}
 				>
-					<div className="w-[95%] md:w-[70%]  xl:w-3/5 mx-auto absolute inset-x-0 bottom-0 transform translate-y-1/2 flex items-end justify-between bg-pink-300 lg:p-3">
+					<div className="w-[95%] md:w-[70%]  xl:w-3/5 mx-auto absolute inset-x-0 bottom-0 transform translate-y-1/2 flex flex-col gap-2 md:flex-row md:items-end md:justify-between lg:p-1">
 						<div
-							className="cursor-pointer rounded-full w-28 md:w-36 xl:w-40 h-28 md:h-36 xl:h-40 bg-center bg-cover bg-no-repeat border-1 border-purple-300"
+							className="cursor-pointer rounded-full w-20 md:w-36 xl:w-40 h-20 md:h-36 xl:h-40 bg-center bg-cover bg-no-repeat border-1 border-purple-300"
 							style={{ backgroundImage: `url(${creator!.imageUrl})` }}
 						></div>
-						<Button>Edit Page</Button>
+						<div className="flex-1 p-3 bg-green-800 flex items-center gap-1.5 md:justify-end">
+							<Button>Edit Profile</Button>
+							<Button>Edit Profile</Button>
+							<Button>Edit Profile</Button>
+						</div>
 					</div>
 				</div>
-				<div className="w-[95%] md:w-[70%] xl:w-3/5 bg-red-400 p-2 /5 mx-auto mt-14 md:mt-20  xl:mt-24">
+				<div className="w-[95%] md:w-[70%] xl:w-3/5 bg-red-400 p-2 /5 mx-auto mt-16 md:mt-20  xl:mt-[5.6rem]">
 					<div>
 						<div>
-							<p>
-								fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-							</p>
+							<p>{creator.userName}</p>
+							<p>{creator.firstName || '' + ' ' + creator.lastName || ''}</p>
 						</div>
 						<div>
 							<p>
 								fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 							</p>
-							<p>
-								fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-							</p>
-							<p>
-								fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-							</p>
 						</div>
+                        <section className="p-10 bg-sky-900"></section>
 					</div>
-					<section className='p-10 bg-sky-900'></section>
 				</div>
 			</section>
 		</main>
