567cacd5e5f1bfe29d035d931d8975b50c74cc5a
sijirama <gbemilesanmi@gmail.com>
2024-06-22 19:35:04 +0100

    gallery fixed

--- app/(admin)/gallery/page.tsx
+++ app/(admin)/gallery/page.tsx
@@ -93,15 +93,12 @@ export default function Page() {
 								const refProp = index === posts.length - 1 ? { ref: lastElementRef } : {};
 								return (
 									<PhotoView key={post.id} src={post.imageUrl}>
-										<>
-											<button>{index}</button>
 											<PostImageComponent
 												{...refProp}
 												imageOnly={false}
 												post={post}
 												lastElementRef={lastElementRef}
 											/>
-										</>
 									</PhotoView>
 								);
 							})}
