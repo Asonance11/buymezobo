04a028c0df0601a8de6eb14487802c0c6058d22e
sijirama <gbemilesanmi@gmail.com>
2024-06-22 18:02:11 +0100

    gallery fixed

--- app/(admin)/gallery/page.tsx
+++ app/(admin)/gallery/page.tsx
@@ -1,69 +1,105 @@
 'use client';
-import { getCreatorPosts } from '@/actions/posts';
 import PostImageComponent from '@/components/Posts/Post';
 import { Button } from '@/components/ui/button';
 import { getCurrentUser } from '@/lib/authentication';
 import { useInterface } from '@/store/InterfaceStore';
 import { Post } from '@prisma/client';
-import { User } from 'lucia';
-import React, { useEffect, useState } from 'react';
-import GallerySection from '@/components/Posts/GallerySection';
-import { useQuery } from '@tanstack/react-query';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import queryKeys from '@/query-key-factory';
-import { toast } from 'sonner';
-import { LoadingOutlined } from '@ant-design/icons';
 import { Separator } from '@/components/ui/separator';
+import { PostPrimitive } from '@/types/primitives';
+import axios from 'axios';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+const MAX_ARTICLES_PAGE = 10;
 
 export default function Page() {
-	const { onOpen } = useInterface();
-	const [latestPost, setLatestPost] = useState<Post[] | null>(null);
+    const { onOpen } = useInterface();
+    const observer = useRef<IntersectionObserver | null>(null);
 
-	const { data: creator } = useQuery({
-		queryKey: queryKeys.user.current(),
-		queryFn: () => getCurrentUser(),
-	});
+    const { data: creator } = useQuery({
+        queryKey: queryKeys.user.current(),
+        queryFn: () => getCurrentUser(),
+    });
 
-	const {
-		data: posts,
-		fetchStatus,
-		status,
-	} = useQuery({
-		queryKey: queryKeys.post.many(),
-		queryFn: () => getCreatorPosts(creator?.id!, 0),
-		enabled: !!creator,
-		refetchOnWindowFocus: false,
-	});
+    const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+        console.log(`Fetching posts for page: ${pageParam}`);
+        const response = await axios.get(
+            `/api/posts/user/${creator?.userName}?page=${pageParam}&limit=${MAX_ARTICLES_PAGE}`,
+        );
+        const posts = response.data.posts || [];
+        console.log(`Fetched posts: `, posts);
+        return posts as PostPrimitive[] | [];
+    };
 
-	useEffect(() => {
-		if (fetchStatus === 'idle' && status === 'success') {
-			setLatestPost(posts);
-		}
-		if (fetchStatus === 'idle' && status === 'error') toast.error('An error occurred while fetching posts');
-	}, [fetchStatus, status]);
+    const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfiniteQuery({
+        queryKey:queryKeys.post.many(),
+        queryFn: ({ pageParam }) => fetchPosts({ pageParam }),
+        getNextPageParam: (lastPage, allPages) => {
+            console.log('Determining next page param...', lastPage, allPages);
+            return lastPage.length === 0 ? undefined : allPages.length + 1;
+        },
+        initialPageParam: 1,
+        enabled: !!creator,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: true,
+    });
 
-	if (fetchStatus === 'fetching')
-		return (
-			<div className=" flex items-center justify-center w-full h-full">
-				<LoadingOutlined />
-			</div>
-		);
+    const lastElementRef = useCallback(
+        (node: HTMLDivElement) => {
+            if (isLoading) return;
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+                    console.log('Fetching next page...');
+                    fetchNextPage();
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [fetchNextPage, hasNextPage, isFetching, isLoading],
+    );
 
-	if (!creator) {
-		return null;
-	}
+    const posts = useMemo(() => {
+        if (!data || !data.pages) return [];
+        return data.pages.flat();
+    }, [data]);
 
-	return (
-		<div className="w-11/12 lg:3/4 xl:w-2/3 mx-auto m-3 lg:my-8">
-			<section className="w-full flex items-center justify-end px-2">
-				<Button
-					onClick={() => onOpen('makeImagePostModal')}
-					className="-tracking-wide text-xs md:text-sm font-bold bg-purple-800"
-				>
-					Make New Post
-				</Button>
-			</section>
-			<Separator className="my-2 md:my-3 lg:my-5 " />
-			<GallerySection posts={latestPost} isImageOnly={false} />
-		</div>
-	);
+    return (
+        <div className="w-11/12 lg:3/4 xl:w-2/3 mx-auto m-3 lg:my-8">
+            <section className="w-full flex items-center justify-end px-2">
+                <Button
+                    onClick={() => onOpen('makeImagePostModal')}
+                    className="-tracking-wide text-xs md:text-sm font-bold bg-purple-800"
+                >
+                    Make New Post
+                </Button>
+            </section>
+            <Separator className="my-2 md:my-3 lg:my-5 " />
+            {posts && posts.length > 0 && (
+
+                <PhotoProvider maskOpacity={0.9}>
+                    <ResponsiveMasonry
+                        columnsCountBreakPoints={{ 300: 2, 500: 3, 700: 4 }}
+                    >
+                        {/*
+                        */}
+                        <Masonry gutter='1rem'>
+                            {posts.map((post, index) => {
+                                const refProp = index === posts.length - 1 ? { ref: lastElementRef } : {};
+                                return (
+                                    <PhotoView key={post.id} src={post.imageUrl}>
+                                        < PostImageComponent {...refProp} imageOnly={false} post={post} />
+                                    </PhotoView>
+                                );
+                            })}
+                        </Masonry>
+                    </ResponsiveMasonry>
+                </PhotoProvider>
+            )}
+        </div>
+    );
 }
+
--- app/(public)/[username]/_tabs/gallery.tsx
+++ app/(public)/[username]/_tabs/gallery.tsx
@@ -4,101 +4,85 @@ import axios from 'axios';
 import { PostPrimitive } from '@/types/primitives';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { Box } from '@mui/material';
-import Masonry from '@mui/lab/Masonry';
 import PostImageComponent from '@/components/Posts/Post';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { LoadingOutlined } from '@ant-design/icons';
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 interface GalleryTabProps {
-	tabIndex: number;
-	creatorname: string;
-	tabValue?: string;
+    tabIndex: number;
+    creatorname: string;
+    tabValue?: string;
 }
 
 const MAX_ARTICLES_PAGE = 10;
 
 export function GalleryTab({ tabIndex, creatorname, tabValue }: GalleryTabProps) {
-	const observer = useRef<IntersectionObserver | null>(null);
-	const masonryRef = useRef<HTMLDivElement>(null);
-	const [isMasonryMounted, setIsMasonryMounted] = useState(false);
-	const [isReturningNull, setIsReturningNull] = useState(false);
+    const observer = useRef<IntersectionObserver | null>(null);
 
-	const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
-		console.log(`Fetching posts for page: ${pageParam}`);
-		const response = await axios.get(`/api/posts/user/${creatorname}?page=${pageParam}&limit=${MAX_ARTICLES_PAGE}`);
-		const posts = response.data.posts || [];
-		console.log(`Fetched posts: `, posts);
-		return posts as PostPrimitive[] | [];
-	};
+    const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+        const response = await axios.get(`/api/posts/user/${creatorname}?page=${pageParam}&limit=${MAX_ARTICLES_PAGE}`);
+        const posts = response.data.posts || [];
+        return posts as PostPrimitive[] | [];
+    };
 
-	const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfiniteQuery({
-		queryKey: ['images', creatorname],
-		queryFn: ({ pageParam }) => fetchPosts({ pageParam }),
-		getNextPageParam: (lastPage, allPages) => {
-			console.log('Determining next page param...', lastPage, allPages);
-			return lastPage.length === 0 ? undefined : allPages.length + 1;
-		},
-		initialPageParam: 1,
-		enabled: tabValue === 'gallery' && !isReturningNull,
-		refetchOnWindowFocus: false,
-		refetchOnReconnect: true,
-	});
+    const { data, fetchNextPage, hasNextPage, isFetching, isLoading } = useInfiniteQuery({
+        queryKey: ['images', creatorname],
+        queryFn: ({ pageParam }) => fetchPosts({ pageParam }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length === 0 ? undefined : allPages.length + 1;
+        },
+        initialPageParam: 1,
+        enabled: tabValue === 'gallery',
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: true,
+    });
 
-	const lastElementRef = useCallback(
-		(node: HTMLDivElement) => {
-			if (isLoading) return;
-			if (observer.current) observer.current.disconnect();
-			observer.current = new IntersectionObserver((entries) => {
-				if (entries[0].isIntersecting && hasNextPage && !isFetching) {
-					console.log('Fetching next page...');
-					fetchNextPage();
-				}
-			});
-			if (node) observer.current.observe(node);
-		},
-		[fetchNextPage, hasNextPage, isFetching, isLoading],
-	);
+    const lastElementRef = useCallback(
+        (node: HTMLDivElement) => {
+            if (isLoading) return;
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+                    console.log('Fetching next page...');
+                    fetchNextPage();
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [fetchNextPage, hasNextPage, isFetching, isLoading],
+    );
 
-	const posts = useMemo(() => {
-		if (!data || !data.pages) return [];
-		return data.pages.flat();
-	}, [data]);
+    const posts = useMemo(() => {
+        if (!data || !data.pages) return [];
+        return data.pages.flat();
+    }, [data]);
 
-	useEffect(() => {
-		if (masonryRef.current) {
-			setIsMasonryMounted(true);
-		}
-	}, []);
+    return (
+        <main className="flex flex-col min-h-96">
+            <div className="flex-1 flex flex-col-reverse bg-gray-100 ">
 
-	if (isReturningNull) {
-		return null;
-	}
+                {posts && posts.length > 0 && (
 
-	return (
-		<main className="flex flex-col min-h-96">
-			<div className="flex-1 flex flex-col-reverse lg:flex-row justify-center gap-3 relative items-center py-5 lg:py-3 lg:items-start bg-gray-100 ">
-				<Box>
-					{/*isMasonryMounted && posts && posts.length > 0 && (
-                                <PhotoProvider maskOpacity={0.9}>
-                                    {posts.map((post, index) => {
-                                        const refProp = index === posts.length - 1 ? { ref: lastElementRef } : {};
-                                        return (
-                                            <PhotoView key={post.id} src={post.imageUrl}>
-                                                <PostImageComponent {...refProp} imageOnly={false} post={post} />
-                                            </PhotoView>
-                                        );
-                                    })}
-                                </PhotoProvider>
-                            ) 
-                        {isFetching && (
-                            <div className="w-full flex items-center justify-center p-3">
-                                <LoadingOutlined />
-                            </div>
-                        )}
+                    <PhotoProvider maskOpacity={0.9}>
+                        <ResponsiveMasonry
+                            columnsCountBreakPoints={{ 300: 2, 500: 3, 700: 3 }}
+                        >
+                            <Masonry gutter='1rem'>
+                                {posts.map((post, index) => {
+                                    const refProp = index === posts.length - 1 ? { ref: lastElementRef } : {};
+                                    return (
+                                        <PhotoView key={post.id} src={post.imageUrl}>
+                                            < PostImageComponent {...refProp} imageOnly={true} post={post} />
+                                        </PhotoView>
+                                    );
+                                })}
+                            </Masonry>
+                        </ResponsiveMasonry>
+                    </PhotoProvider>
+                )}
 
-                            */}
-				</Box>
-			</div>
-		</main>
-	);
+            </div>
+        </main>
+    );
 }
